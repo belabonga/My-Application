4315b7aa73d49159a9abc93895143782bb19f4c3
belabonga <shania1faza@gmail.com>
2016-09-08 20:06:23 +0700

    ~ Second Commit ~
    Mengoding "Nama" agar muncul

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3033/myapplication/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3033/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3033.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -11,7 +12,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvHasil;
-    EditText etNama, etTempat, etTahun;
+    EditText etTempat, etTahun;
+    EditText etNama;
     Button bSubmit;
     RadioButton rbKK, rbRS, rbROT;
     CheckBox cbP, cbNP, cbTSS;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvHasil = (TextView) findViewById(R.id.textView);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         etTahun = (EditText) findViewById(R.id.editTextTahun);
@@ -37,6 +39,37 @@ public class MainActivity extends AppCompatActivity {
         cbP = (CheckBox) findViewById(R.id.checkBoxP);
         cbTSS = (CheckBox) findViewById(R.id.checkBoxTSS);
 
-
+        bSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doProcess();
+            }
+        });
     }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            tvHasil.setText("Nama   :  " + nama);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        return valid;
+    }
+
 }
